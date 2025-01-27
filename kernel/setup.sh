a37136ff2e4d9abf1ae2ead399756ed8b6ec7e4e
Nathan Andrew <nathannxx@proton.me>
2025-01-27 19:35:34 +0700

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -43,7 +43,7 @@ setup_kernelsu() {
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout susfs-4.9 && echo "[-] Switched to next branch."
+        git checkout susfs-4.9 && echo "[-] Switched to susfs-4.9 branch."
     fi
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
